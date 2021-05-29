f90f0ca2a25bfe4a2f4de8433ad11b70293e2e98
zhu.jiang <zhu.jiang@56qq.com>
2021-05-29 21:20:55 +0800

    使用最近30条数据

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,15 @@
 class Article < ApplicationRecord
   belongs_to :target
-  scope :lately_on, -> { order(created_at: :asc).limit(30) }
+  scope :lately_on, -> { order(created_at: :desc).limit(30) }
   scope :created_on, -> { where(created_at: DateTime.now.beginning_of_day..DateTime.now.end_of_day) }
   
   def self.today
-    self.created_on
+    created = self.created_on
+    if created.empty?
+      self.lately_on # 当天没数据返回最近30条数据
+    else
+      created
+    end
   end
   def self.lately
     self.lately_on
